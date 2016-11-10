6431e27ee0133949510689be4f5ef1aedbe84765
Christoforos Demetriou <cdemet08@cs.ucy.ac.cy>
2016-11-10 13:18:31 +0200

    add login file and correct config file

--- HTML/includes/db_connect.php
+++ HTML/includes/db_connect.php
@@ -1,10 +1,9 @@
 
 <?php
 
-include_once 'psl-config.php';   // As functions.php is not included
-$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
-
-//TODO fix correct ?? what 
-?>
-
+include_once 'db_config.php';   // As functions.php is not included
+if ($mysqli->connect_error) {
+    header("Location: ../error.php?err=Unable to connect to MySQL");
+    exit();
+}
 
