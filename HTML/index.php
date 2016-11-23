c180b8937a9549b0fd0a79bf0dc6fab5e5dc8808
Christoforos Demetriou <cdemet08@cs.ucy.ac.cy>
2016-11-23 10:36:40 +0200

    rename php file

--- HTML/index.php
+++ HTML/index.php
@@ -1,4 +1,25 @@
 
+
+<?php
+
+#include file for php to run 
+
+include_once 'includes/db_connect.php';
+include_once 'includes/functions.php';
+
+
+sec_session_start();
+
+if (login_check($mysqli) == true) {
+    $logged = 'in';
+    header('Location: ../user.php');
+
+} else {
+    $logged = 'out';
+}
+
+?>
+
 <html lang="en-us">
 <head>
     <meta charset="UTF-8">
@@ -15,7 +36,11 @@
    </head>
 <body>
 
-
+    <?php
+    if (isset($_GET['error'])) {
+      echo '<p class="error">Error Logging In!</p>';
+    }
+    ?> 
 
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
        <div class="container-fluid">
@@ -28,7 +53,7 @@
                     <span class="icon-bar"></span>
                </button>
 
-                <a  class="navbar-brand" ><img style="display: inline-block; height: 30px; margin-top: -5px"   src="img/logo.jpg"> </a>
+                <a  class="navbar-brand" ><img style="display: inline-block; height: 30px; margin-top: -5px"   src="img/logo.png"> </a>
                <a id="Name">Hair Salon</a>
             </div>
            <div class="navbar-collapse collapse">
@@ -53,37 +78,37 @@
 
            <div id="LoginForm" class="modal">
 
-               <div class="modal-content animate" action="action_page.php">
+               <div class="modal-content animate" "includes/login_function.php" method="post" name="login_form"> 
 
-                       <div class="imgcontainer">
-                            <span onclick="document.getElementById('LoginForm').style.display='none'" class="close" title="Close Modal">&times;</span>
-                            <img src="img/avatar.png" alt="Avatar" class="avatar">
+                 <div class="imgcontainer">
+                      <span onclick="document.getElementById('LoginForm').style.display='none'" class="close" title="Close Modal">&times;</span>
+                      <img src="img/avatar.png" alt="Avatar" class="avatar">
 
-                        </div>
-
-
-                                    <div class="form-group">
-                                        <label id="email" class="control-label col-sm-2" for="usr">Email:</label>
-                                        <div class="col-sm-10">
-                                            <input type="text" style="height: 3%" placeholder="Enter your Email" class="form-control" id="usr">
-                                         </div>
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label id="pass"class="control-label col-sm-2" for="pwd">Password:</label>
-                                        <div class="col-sm-10">
-                                            <input type="password"  style="height: 3%" class="form-control" placeholder="Enter your Password"id="pwd">
-                                        </div>
-                                    </div>
-                                    <div class="row">
-
-                                            <a href="#"><input  id="remember" type="checkbox" checked="checked"> Remember me</a>
-                                            <a href="#"><button id="submitButton" type="submit" >Login</button></a>
-
-                                     </div>
+                  </div>
 
 
-               </div>
+                  <div class="form-group">
+                    <label id="email" class="control-label col-sm-2" for="usr">Email:</label>
+                    <div class="col-sm-10">
+                        <input type="text" style="height: 3%" placeholder="Enter your Email" class="form-control" id="usr">
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <label id="pass"class="control-label col-sm-2" for="pwd">Password:</label>
+                    <div class="col-sm-10">
+                        <input type="password"  style="height: 3%" class="form-control" placeholder="Enter your Password"id="pwd">
+                    </div>
+                  </div>
+                  <div class="row">
+
+                    <a href="#"><input  id="remember" type="checkbox" checked="checked"> Remember me</a>
+                    <a href="#"><button id="submitButton" type="submit" >Login</button></a>
+
+                </div>
+
+
+              </div>
 
            </div>
        </div>
@@ -92,45 +117,45 @@
 
 
 
-                <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                 <!-- Indicators -->
-                    <ol class="carousel-indicators">
-                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                    <li data-target="#myCarousel" data-slide-to="1"></li>
-                    <li data-target="#myCarousel" data-slide-to="2"></li>
-                    <li data-target="#myCarousel" data-slide-to="3"></li>
-                    </ol>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+     <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+          <li data-target="#myCarousel" data-slide-to="3"></li>
+        </ol>
 
-            <!-- Wrapper for slides -->
-                    <div class="carousel-inner" role="listbox" >
+  <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox" >
 
-                        <div class="item active" >
-                            <img src="http://victoriassalon.com/wp-content/uploads/2015/02/rev.jpg" alt="HairSalon" width="100%" height="60%">
-                        <div class="carousel-caption">
-                            <h3>HairSalon</h3>
-                            <p>The atmosphere in Hair Salon has a touch of Relaxing </p>
-                        </div>
-                    </div>
-
-
-
-                     <!-- Left and right controls -->
-                     <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                       <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                         <span class="sr-only">Previous</span>
-                     </a>
-                     <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                         <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                         <span class="sr-only">Next</span>
-                     </a>
-                  </div>
-                </div>
+            <div class="item active" >
+                <img src="http://victoriassalon.com/wp-content/uploads/2015/02/rev.jpg" alt="HairSalon" width="100%" height="60%">
+            <div class="carousel-caption">
+                <h3>HairSalon</h3>
+                <p>The atmosphere in Hair Salon has a touch of Relaxing </p>
+            </div>
+        </div>
 
 
+
+         <!-- Left and right controls -->
+         <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+           <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+             <span class="sr-only">Previous</span>
+         </a>
+         <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+             <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+             <span class="sr-only">Next</span>
+         </a>
+      </div>
     </div>
 
 
+  </div>
 
+
+ 
 
 
 </body>
