bc42711db1b52e5b0735147228e2dc077f139f27
Christoforos Demetriou <cdemet08@cs.ucy.ac.cy>
2016-11-25 12:32:45 +0200

    add site for register success and redirect the site to index.php

---
+++ HTML/register_success.php
@@ -0,0 +1,22 @@
+<html>
+
+<head>
+
+<title>Page Auto Redirect</title>
+
+<script language="javascript" type="text/javascript">
+
+	window.setTimeout('window.location="index.php"; ',2000);
+
+			
+</script>
+
+</head>
+
+<body>
+
+This is an auto redirect page.
+
+</body>
+
+</html>
